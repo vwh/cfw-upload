6bf31f1fa9b405ccb35a1a412b3c147ef4e377a7
vwh <vwhe@proton.me>
2024-09-15 14:05:08 +0300

    fix: fixing .env

--- src/lib/bucket.ts
+++ src/lib/bucket.ts
@@ -1,9 +1,18 @@
 import { R2 } from "node-cloudflare-r2";
+import "dotenv/config";
+
+function getEnv(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`Missing environment variable ${key}`);
+  }
+  return value;
+}
 
 const r2 = new R2({
-  accountId: process.env.R2_ACCOUNT_ID!,
-  accessKeyId: process.env.R2_ACCESS_KEY_ID!,
-  secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
+  accountId: getEnv("R2_ACCOUNT_ID"),
+  accessKeyId: getEnv("R2_ACCESS_KEY_ID"),
+  secretAccessKey: getEnv("R2_SECRET_ACCESS_KEY"),
 });
 
-export const bucket = r2.bucket(process.env.R2_BUCKET_NAME!);
+export const bucket = r2.bucket(getEnv("R2_BUCKET_NAME"));
