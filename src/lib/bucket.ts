9b130685bec9317eeef8eefb13882e9f78959536
vwh <vwhe@proton.me>
2024-09-15 15:47:00 +0300

    feat: added wrangler

--- src/lib/bucket.ts
+++ src/lib/bucket.ts
@@ -1,18 +1,10 @@
 import { R2 } from "node-cloudflare-r2";
-import "dotenv/config";
-
-function getEnv(key: string) {
-  const value = process.env[key];
-  if (!value) {
-    throw new Error(`Missing environment variable ${key}`);
-  }
-  return value;
-}
+import { env } from "node:process";
 
 const r2 = new R2({
-  accountId: getEnv("R2_ACCOUNT_ID"),
-  accessKeyId: getEnv("R2_ACCESS_KEY_ID"),
-  secretAccessKey: getEnv("R2_SECRET_ACCESS_KEY"),
+  accountId: env.R2_ACCOUNT_ID!,
+  accessKeyId: env.R2_ACCESS_KEY_ID!,
+  secretAccessKey: env.R2_SECRET_ACCESS_KEY!,
 });
 
-export const bucket = r2.bucket(getEnv("R2_BUCKET_NAME"));
+export const bucket = r2.bucket(env.R2_BUCKET_NAME!);
